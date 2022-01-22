7a760d728cdbae801a07d2f399ae9cf7ac87273f
chrislantz <chrishuberlantz@gmail.com>
2022-01-21 19:31:48 -0800

    fix tests

--- apps/dnd-board/src/app/login/login.component.spec.ts
+++ apps/dnd-board/src/app/login/login.component.spec.ts
@@ -1,3 +1,5 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
@@ -8,9 +10,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+      ],
+      declarations: [ LoginComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- apps/dnd-board/src/app/login/login.component.ts
+++ apps/dnd-board/src/app/login/login.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  public password: string;
-  public username: string;
+  public password: string = '';
+  public username: string = '';
 
   constructor() { }
 
